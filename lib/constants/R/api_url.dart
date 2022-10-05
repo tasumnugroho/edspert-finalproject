7a116424884505157d26183318493675ec62120a
tasumnugroho <nugroho.gatot04@gmail.com>
2022-10-05 14:26:34 +0700

    Endpoint Api

---
+++ lib/constants/R/api_url.dart
@@ -0,0 +1,12 @@
+class ApiUrl {
+  static String baseUrl = "https://ekskul.id/api/";
+  static String users = "users";
+  static String usersRegistrasi = "users/registrasi";
+  static String usersUpdateProfile = "users/update";
+
+  static String latihanMapel = "exercise/data_course";
+  static String latihanPaketSoal = "";
+  static String latihanKerjakanSoal = "exercise/kerjakan";
+  static String latihanSubmitJawaban = "exercise/input_jawaban";
+  static String latihanSkor = "exercise/score_result";
+}
